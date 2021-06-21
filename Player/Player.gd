e960528b0cd255771de98227404f6262ab98737b
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-06-21 15:10:08 -0300

    normalizing the velocity

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 400
+const MAX_SPEED = 100
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")#gets the datas of right and left arrow keys
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")#gets the datas of down and up arrow keys
+	input_vector = input_vector.normalized()#makes the character runs at the same speed in all directions	
 	
 	if input_vector != Vector2.ZERO:
 		velocity = input_vector 
