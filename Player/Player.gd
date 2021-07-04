6d4a9e1aa8c091cc58bfbc88ff374307cecc066c
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-04 16:25:39 -0300

    Setting the enum to match the functions

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,15 @@ const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
 
+enum{#its like an array	
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 var velocity = Vector2.ZERO
+var state =  MOVE
+
 onready var animationPlayer = $AnimationPlayer #Turn the animations into a variable inside the _ready funcion
 onready var animationTree = $AnimationTree #Turn the animations Tree into a variable
 onready var animationState = animationTree.get("parameters/playback")
@@ -13,6 +21,15 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")#gets the datas of right and left arrow keys
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")#gets the datas of down and up arrow keys
@@ -31,3 +48,6 @@ func _physics_process(delta):
 	
 	print(velocity) 
 	velocity = move_and_slide(velocity)
+
+func attack_state(delta):
+	pass
