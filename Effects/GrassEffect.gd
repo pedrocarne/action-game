dd38732a27ccacb4b5c21173babec81b566d8abf
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-12 19:40:25 -0300

    removing the attack func from GE

--- Effects/GrassEffect.gd
+++ Effects/GrassEffect.gd
@@ -3,9 +3,5 @@ extends Node2D
 onready var animatedSprite = $AnimatedSprite
 
 func _ready():
+	animatedSprite.frame = 0
 	animatedSprite.play("Animate")
-	
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		animatedSprite.frame = 0
-		animatedSprite.play("Animate")
