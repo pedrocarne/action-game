d69b7d5d2e4459771fff036cd5c90bc5f91956c1
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-12 20:09:01 -0300

    setting up the grass to be destroyed and create the effect

--- Effects/GrassEffect.gd
+++ Effects/GrassEffect.gd
@@ -5,3 +5,7 @@ onready var animatedSprite = $AnimatedSprite
 func _ready():
 	animatedSprite.frame = 0
 	animatedSprite.play("Animate")
+
+
+func _on_AnimatedSprite_animation_finished():
+	queue_free()
