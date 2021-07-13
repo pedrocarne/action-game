75d3832c7bb884b22281ed328e599fc3ddd612a7
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-12 21:04:01 -0300

    setting up the hitbox in the player

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position #sets the grassE gb to grass gb
-		
-		queue_free() #destroy the scene
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position #sets the grassE gb to grass gb
+	
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
