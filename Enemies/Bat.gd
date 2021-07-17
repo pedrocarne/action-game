6f01b316c8eb89beb9b18def6bb6e2f8792a96b0
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-16 21:23:23 -0300

    making the bat desappear when its health is 0

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,10 +2,18 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats #takes the Stats Scene to a variable
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	print(stats.health);
 	knockback = area.knockback_vector * 110
-	#queue_free()
+	
+
+
+func _on_Stats_no_health():
+	queue_free()
--- Stats.gd
+++ Stats.gd
@@ -2,4 +2,13 @@ extends Node
 
 
 export var max_health = 1 #exports the var to the editor
-onready var health = max_health #the health only changes to the editor set if its in the ready func 
+onready var health = max_health setget set_health #set this func every time the variable gets set
+#the health only changes to the editor set if its in the ready func 
+
+signal no_health
+
+func set_health(value):
+	health = value
+	if health <= 0:
+		emit_signal("no_health")
+	
