90ec74b09ffc7b3380c5b819b29f8fdab6fb1c91
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-17 21:46:54 -0300

    making the bats move arround

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -16,7 +16,9 @@ var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 var state = IDLE
 
+onready var sprite = $Sprite
 onready var stats = $Stats #takes the Stats Scene to a variable
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -25,13 +27,20 @@ func _physics_process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
 		WANDER:
 			pass
 		CHASE:
-			pass
-
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
+	
 func seek_player():
-	pass
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
