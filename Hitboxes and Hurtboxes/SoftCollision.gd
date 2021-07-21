b886aa37d2fbfe2bd633a69679d3aa5a626760fd
Pedro <pedrohenriquecarneiro143@gmail.com>
2021-07-20 23:11:59 -0300

    adding the script to the soft collision

--- Hitboxes and Hurtboxes/SoftCollision.gd
+++ Hitboxes and Hurtboxes/SoftCollision.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
-func is_colliding():
+func is_colliding(): #gets every area that is colliding but it has to be greater than 0
 	var areas = get_overlapping_areas()
 	return areas.size() > 0
+	
+func get_push_vector():
+	var areas = get_overlapping_areas()
+	var push_vector = Vector2.ZERO
+	if is_colliding():
+		var area = areas[0] #gets the first area that is colliding and ignore the others
+		push_vector = area.global_position.direction_to(global_position)#the vector goes from its positiion to player position
+		push_vector = push_vector.normalized()
+	return push_vector
